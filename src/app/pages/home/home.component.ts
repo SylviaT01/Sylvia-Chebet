6597386b0cebf4d137efdd61504b747645d0ff6e
Sylvia Chebet <sylviachebet03@gmail.com>
2025-05-06 10:02:16 +0300

    Create About section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,55 @@
+// import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+// import { isPlatformBrowser } from '@angular/common';
+// import { Router, NavigationEnd } from '@angular/router';
+// import { filter } from 'rxjs/operators';
+// const Sylvia = 'assets/images/Sylvia.jpg';
+
+// @Component({
+//   selector: 'app-home',
+//   standalone: false,
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.scss'
+// })
+// export class HomeComponent implements OnInit {
+//   public imageSrc = Sylvia;
+//   private isBrowser: boolean;
+
+//   constructor(
+//     private router: Router,
+//     @Inject(PLATFORM_ID) platformId: Object
+//   ) {
+//     this.isBrowser = isPlatformBrowser(platformId);
+//   }
+
+//   ngOnInit() {
+//     if (this.isBrowser) {
+//       import('aos').then(AOS => {
+//         AOS.init({
+//           duration: 800,
+//           easing: 'ease-in-out',
+//           once: false
+//         });
+
+//         this.router.events
+//           .pipe(filter(event => event instanceof NavigationEnd))
+//           .subscribe(() => {
+//             setTimeout(() => AOS.refreshHard(), 100);
+//           });
+//       });
+//     }
+//   }
+// }
 import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-const Sylvia = 'assets/images/Sylvia.jpg';
+const Sylvia = 'assets/images/Sylvia-profile.png';
 
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styles: []
 })
 export class HomeComponent implements OnInit {
   public imageSrc = Sylvia;
