aa45a918fba58a071e3c6ab831ae0ecd61c638c3
Sylvia Chebet <sylviachebet03@gmail.com>
2025-05-07 12:03:31 +0300

    Edit home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,44 +1,3 @@
-// import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
-// import { isPlatformBrowser } from '@angular/common';
-// import { Router, NavigationEnd } from '@angular/router';
-// import { filter } from 'rxjs/operators';
-// const Sylvia = 'assets/images/Sylvia.jpg';
-
-// @Component({
-//   selector: 'app-home',
-//   standalone: false,
-//   templateUrl: './home.component.html',
-//   styleUrl: './home.component.scss'
-// })
-// export class HomeComponent implements OnInit {
-//   public imageSrc = Sylvia;
-//   private isBrowser: boolean;
-
-//   constructor(
-//     private router: Router,
-//     @Inject(PLATFORM_ID) platformId: Object
-//   ) {
-//     this.isBrowser = isPlatformBrowser(platformId);
-//   }
-
-//   ngOnInit() {
-//     if (this.isBrowser) {
-//       import('aos').then(AOS => {
-//         AOS.init({
-//           duration: 800,
-//           easing: 'ease-in-out',
-//           once: false
-//         });
-
-//         this.router.events
-//           .pipe(filter(event => event instanceof NavigationEnd))
-//           .subscribe(() => {
-//             setTimeout(() => AOS.refreshHard(), 100);
-//           });
-//       });
-//     }
-//   }
-// }
 import { Component, OnInit, PLATFORM_ID, Inject, AfterViewInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
@@ -64,6 +23,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
     if (this.isBrowser) {
       import('aos').then(AOS => {
         this.AOS = AOS;
@@ -78,15 +40,11 @@ export class HomeComponent implements OnInit, AfterViewInit {
           .subscribe(() => {
             setTimeout(() => AOS.refreshHard(), 100);
           });
-      });
-    }
-  }
 
-  ngAfterViewInit() {
-    if (this.isBrowser && this.AOS) {
-      setTimeout(() => {
-        this.AOS.refreshHard();
-      }, 200);
+        setTimeout(() => {
+          this.AOS.refreshHard();
+        }, 200);
+      });
     }
   }
 }
